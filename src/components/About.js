76be94284ef7a1c1ace93f59773b0d6756f85528
Sarita <saritasp204@gmail.com>
2023-12-07 14:05:23 +0530

    about us page added

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,9 @@ import img3 from "../images/img3.png";
 import aboutimg1 from "../images/aboutimg1.png";
 import aboutimg2 from "../images/aboutimg2.png";
 import aboutimg3 from "../images/aboutimg3.png";
+import benefitimg1 from "../images/benefitimg1.png";
+import benefitimg2 from "../images/benefitimg2.png";
+import benefitimg3 from "../images/benefitimg3.png";
 
 function About() {
     return (
@@ -17,7 +20,7 @@ function About() {
                 <h2 className="banner-main-title">OUR ACHIEVMENTS</h2>
             </div>
 
-            <div className="home-page-service-section">
+            <div className="home-page-service-section" style={{ paddingBottom: "50px", background: "none" }}>
                 <div className="service-title-section">
                     <h6>WELCOME TO OUR WEBSITE</h6>
                     <h1 style={{ paddingBottom: "20px" }}>WE ARE A HINDU THAT BELIVES IN<br></br>LORD RAMA AND VISHNU DEVA</h1>
@@ -51,6 +54,45 @@ function About() {
                         <h5 class="card-title" className='about-card-right-title'>OUR VOLUNTEERS ACHIEVMENTS</h5>
                         <p class="card-text" className='about-card-right-tagline'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
                     </div>
+                </div>
+            </div>
+
+            <div className='benefit-component'>
+                <div className="service-title-section">
+                    <h6>BENEFIT</h6>
+                    <h1>THE BENEFITS OF JOINING OUR TEMPLE</h1>
+                </div>
+
+                <div className='benefit-cards'>
+                <div className='benefit-card-components'>
+                    <div className='content-caontainer'>
+                        <h5 class="card-title" className='about-card-title'>DONATE EVENTS</h5>
+                        <p class="card-text" className='about-card-tagline'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                    </div>
+                    <div className='image-caontainer'>
+                        <img src={benefitimg1} alt="logo" className='benefit-images' />
+                    </div>
+
+                </div>
+                <div className='benefit-card-components'>
+                <div className='image-caontainer'>
+                        <img src={benefitimg2} alt="logo" className='benefit-images' />
+                    </div>
+                    <div className='content-caontainer'>
+                        <h5 class="card-title" className='about-card-title' style={{textAlign:"justify"}}>ALL ARE WELCOME</h5>
+                        <p class="card-text" className='about-card-tagline' style={{textAlign:"justify"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                    </div>               
+                </div>
+                <div className='benefit-card-components'>
+                    <div className='content-caontainer'>
+                        <h5 class="card-title" className='about-card-title'>FIND FULFILLMENT AND JOY</h5>
+                        <p class="card-text" className='about-card-tagline'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                    </div>
+                    <div className='image-caontainer'>
+                        <img src={benefitimg3} alt="logo" className='benefit-images' />
+                    </div>
+
+                </div>
                 </div>
             </div>
         </div>
